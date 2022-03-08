23f3d687b8cd9728ae42611cd4432312556e251d
gdevanshu709 <gdevanshu709@gmail.com>
2022-03-08 15:20:49 +0530

    jest added

---
+++ server/test/config.ts
@@ -0,0 +1,9 @@
+const testUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@email.com',
+    phoneNumber: '1234567890',
+    password: 'Password@123',
+}
+
+export { testUser }
---
+++ server/test/index.spec.ts
@@ -0,0 +1,100 @@
+/* eslint-disable no-undef */
+import { ApolloServer, gql } from 'apollo-server'
+import { typeDefs, resolvers } from '../src/graphql/schema'
+import { testUser } from './config'
+
+it('returns list of users', async () => {
+    const testServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    })
+
+    const result = await testServer.executeOperation({
+        query: 'query FetchUsers { users { id, firstName, lastName, email, phoneNumber, password } }',
+    })
+
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.users).toBeTruthy()
+    expect(result.data?.users.length).toBeGreaterThan(0)
+})
+
+it('should create new user', async () => {
+    const testServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    })
+
+    const result = await testServer.executeOperation({
+        query: `
+      mutation AddUser(
+        $email: String!
+        $firstName: String!
+        $password: String!
+        $lastName: String!
+        $phoneNumber: String!
+      ) {
+        createUser(
+          firstName: $firstName
+          lastName: $lastName
+          email: $email
+          phoneNumber: $phoneNumber
+          password: $password
+        ) {
+          token
+        }
+      }
+    `,
+        variables: {
+            email: testUser.email,
+            firstName: testUser.firstName,
+            password: testUser.password,
+            lastName: testUser.lastName,
+            phoneNumber: testUser.phoneNumber,
+        },
+    })
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.createUser?.token).toBeTruthy()
+})
+it('should return token', async () => {
+    const testServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    })
+
+    const result = await testServer.executeOperation({
+        query: `mutation LoginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          token
+        }
+      }`,
+        variables: {
+            email: testUser.email,
+            password: testUser.password,
+        },
+    })
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.login?.token).toBeTruthy()
+})
+
+it('should delete created user', async () => {
+    const testServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    })
+
+    const result = await testServer.executeOperation({
+        query: `
+    mutation DeleteUserByEmail($email: String!) {
+      deleteUserByEmail(email: $email) {
+        email
+      }
+    }
+  `,
+        variables: {
+            email: testUser.email,
+        },
+    })
+
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.deleteUserByEmail?.email).toBeTruthy()
+})
